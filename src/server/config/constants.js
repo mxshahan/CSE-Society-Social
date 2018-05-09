cbc8e155cd75f7aa0429b094a1ddf23d098a3117
Shahan Chowhdury <shahan953@hotmail.com>
2018-05-10 04:32:49 +0600

    final commit

--- src/server/config/constants.js
+++ src/server/config/constants.js
@@ -1,19 +1,19 @@
 // MONGO_URL: 'mongodb:/root:12345@ds117540.mlab.com:17540/siu-final'
 
 const devConfig = {
-    MONGO_URL: "mongodb://root:12345@ds119490.mlab.com:19490/siufinal",
-    // MONGO_URL: 'mongodb://localhost:27017/shuvojit02',
+    // MONGO_URL: "mongodb://root:12345@ds119490.mlab.com:19490/siufinal",
+    MONGO_URL: 'mongodb://localhost:27017/shuvojit02',
     JWT_SECRET: 'thisissecret'
 };
 
 const testConfig = {
-    MONGO_URL: "mongodb://root:12345@ds119490.mlab.com:19490/siufinal"
-    // MONGO_URL: 'mongodb://localhost:27017/shuvojit02'
+    // MONGO_URL: "mongodb://root:12345@ds119490.mlab.com:19490/siufinal"
+    MONGO_URL: 'mongodb://localhost:27017/shuvojit02'
 };
 
 const prodConfig = {
-    MONGO_URL: "mongodb://root:12345@ds119490.mlab.com:19490/siufinal"
-    // MONGO_URL: 'mongodb://localhost:27017/shuvojit02'
+    // MONGO_URL: "mongodb://root:12345@ds119490.mlab.com:19490/siufinal"
+    MONGO_URL: 'mongodb://localhost:27017/shuvojit02'
 };
 
 const defaultConfig = {
