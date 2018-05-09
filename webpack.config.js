0a5ca6dfa7a039174d0b0eb07007bafad0a38167
Shahan Chowhdury <shahan953@hotmail.com>
2018-05-10 02:46:31 +0600

    done

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,41 @@
 const path = require('path');
 const webpack = require('webpack');
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const SRC = path.resolve(__dirname, 'src');
 const DST = path.resolve(__dirname, 'public');
-
+const { BaseHrefWebpackPlugin } = require('base-href-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const baseHref = '/';
 module.exports = {
     entry: SRC + '/client/app.js',
     output: {
-        path: DST,
+        path: DST + '/dist',
         filename: 'bundle.js'
     },     
     module: {
         rules: [{
             test: /\.(js|jsx)$/,
             loader: 'babel-loader',
-            exclude: '/node_modules/'
+            exclude: '/node_modules/',
+            query: {compact: false} 
         }, {
             test: /\.(scss|css)$/,
             use: [
                 'style-loader',
                 'css-loader',
                 'sass-loader'
-            ] 
+            ]
         }]
     },
     plugins:[
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': '"production"'
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin(),
+        new BaseHrefWebpackPlugin({baseHref: baseHref})
     ],
-    mode: 'development',    
+    mode: 'production',    
     devtool: 'source-map',
     devServer: {
         contentBase: DST,
